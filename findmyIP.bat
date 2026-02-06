33dd1545b9965762e4a67503810e3ab294891b0c
M4lwa4r3 Mus4shi <113495054+MalwareMusashi@users.noreply.github.com>
2026-02-06 11:54:23 -0700

    Add files via upload

---
+++ findmyIP.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Display header
+echo -------------------------
+echo Device Network Info
+echo -------------------------
+
+:: Get first IPv4 address
+for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /C:"IPv4 Address"') do (
+  set "IP=%%a"
+  goto :foundip
+)
+:foundip
+set "IP=!IP:~1!"
+
+:: Get first MAC address
+for /f "tokens=2 delims=:" %%a in ('getmac /v /fo list ^| findstr /C:"Physical Address"') do (
+  set "MAC=%%a"
+  goto :foundmac
+)
+:foundmac
+set "MAC=!MAC:~1!"
+
+:: Show results
+if defined IP (echo IPv4 Address : %IP%) else (echo IPv4 Address : Not Found)
+if defined MAC (echo MAC Address  : %MAC%) else (echo MAC Address  : Not Found)
+
+echo -------------------------
+pause
+endlocal
