c6e371aa505a0837fa65e84ac298816c6e368835
M4lwa4r3 Mus4shi <113495054+MalwareMusashi@users.noreply.github.com>
2026-02-11 15:05:04 -0700

    Enhance findmyIP.bat to support multiple network adapters
    Refactor script to display network information for all adapters, including hostname, IPv4 addresses, and MAC addresses.

--- findmyIP.bat
+++ findmyIP.bat
@@ -1,34 +1,67 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Display header
-echo -------------------------
+echo ==============================
 echo Device Network Info
-echo -------------------------
+echo ==============================
+echo Hostname : %COMPUTERNAME%
+echo ==============================
 
-:: Get hostname
-echo Hostname     : %COMPUTERNAME%
+set count=0
+set "cname="
+set "cip="
+set "cmac="
 
-:: Get first IPv4 address
-for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /C:"IPv4 Address"') do (
-  set "IP=%%a"
-  goto :foundip
+for /f "usebackq delims=" %%a in (`ipconfig /all`) do (
+  set "line=%%a"
+
+  :: New adapter block detected
+  echo !line! | findstr /r "^[A-Za-z]" >nul 2>&1
+  if !errorlevel!==0 (
+    if defined cip if defined cmac (
+      set /a count+=1
+      set "NAME[!count!]=!cname!"
+      set "IP[!count!]=!cip!"
+      set "MAC[!count!]=!cmac!"
+    )
+    set "cname=!line!"
+    set "cip="
+    set "cmac="
+  )
+
+  :: Grab IPv4
+  echo !line! | findstr /C:"IPv4 Address" >nul 2>&1
+  if !errorlevel!==0 (
+    for /f "tokens=2 delims=:" %%b in ("!line!") do set "cip=%%b"
+    set "cip=!cip:~1!"
+    set "cip=!cip:(Preferred)=!"
+    set "cip=!cip: =!"
+  )
+
+  :: Grab MAC
+  echo !line! | findstr /C:"Physical Address" >nul 2>&1
+  if !errorlevel!==0 (
+    for /f "tokens=2 delims=:" %%b in ("!line!") do set "cmac=%%b"
+    set "cmac=!cmac:~1!"
+  )
 )
-:foundip
-set "IP=!IP:~1!"
 
-:: Get first MAC address
-for /f "tokens=2 delims=:" %%a in ('getmac /v /fo list ^| findstr /C:"Physical Address"') do (
-  set "MAC=%%a"
-  goto :foundmac
+:: Catch last adapter
+if defined cip if defined cmac (
+  set /a count+=1
+  set "NAME[!count!]=!cname!"
+  set "IP[!count!]=!cip!"
+  set "MAC[!count!]=!cmac!"
 )
-:foundmac
-set "MAC=!MAC:~1!"
 
-:: Show results
-if defined IP (echo IPv4 Address : %IP%) else (echo IPv4 Address : Not Found)
-if defined MAC (echo MAC Address  : %MAC%) else (echo MAC Address  : Not Found)
+:: Print results
+for /l %%i in (1,1,%count%) do (
+  echo !NAME[%%i]!
+  echo ------------------------------
+  echo IPv4 Address : !IP[%%i]!
+  echo MAC Address  : !MAC[%%i]!
+  echo ==============================
+)
 
-echo -------------------------
 pause
 endlocal
